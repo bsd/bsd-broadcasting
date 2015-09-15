f4d9cc6df44bffdc81a74b99e0082f9fa34cde22
ghlost <john.moore1224@gmail.com>
2015-09-15 16:57:22 -0700

    Commiting last updates

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # bsd-broadcasting
+This site is built on the Tools so the folders currently match the directories for the Tools.
--- page/-/wrapper/scripts/main.js
+++ page/-/wrapper/scripts/main.js
@@ -1,3 +1,21 @@
-jQuery(document).ready(function() {
-  jQuery('form').h5Validate();
+jQuery(document).ready(function($) {
+  $('form').h5Validate();
+  // handle social share buttons
+  $('.button--share').on('click', function(e) {
+    e.preventDefault();
+    var $target = $(e.currentTarget),
+      url;
+    if ($target.data('tweet') && $target.data('tweet') !== '') {
+      var tweet = $target.data('tweet');
+      url = 'https://www.twitter.com/intent/tweet?text=' + encodeURIComponent(tweet);
+    } else if ($target.data('share') && $target.data('share') !== '') {
+      var shareLink = $target.data('share');
+      url = 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(shareLink);
+    }
+    if (url) {
+      window.open(url, '','menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=600');
+    } else {
+      return false;
+    }
+  });
 });
